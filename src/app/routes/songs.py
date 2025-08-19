26488acd1d426659932729e7057ca28e2621e2c2
ansergio <ansergio@hotmail.com>
2025-08-19 18:04:00 -0300

    Correção de Songs

--- src/app/routes/songs.py
+++ src/app/routes/songs.py
@@ -8,6 +8,30 @@ router = APIRouter()
 
 
 @router.get("/songs")
+def get_song(file: str):
+    path = os.path.join(songs_dir, file)
+
+    if not os.path.isfile(path):
+        return {"error": "Arquivo não encontrado"}
+
+    return FileResponse(path, media_type="audio/mpeg", filename=file)
+    # fallback: envia tudo
+    # return FileResponse(path, media_type="audio/mpeg", headers={"Accept-Ranges": "bytes"})
+
+
+@router.get("/songs/list")
+def list_songs():
+    files = []
+    for root, _, filenames in os.walk(songs_dir):
+        for f in filenames:
+            if f.endswith(".mp3"):
+                rel_path = os.path.relpath(os.path.join(root, f), songs_dir)
+                files.append(rel_path)
+    return {"songs": files}
+
+
+"""
+@router.get("/songs")
 def get_song(file: str, request: Request):
     path = os.path.join(songs_dir, file)
 
@@ -40,19 +64,6 @@ def get_song(file: str, request: Request):
     # fallback: envia tudo
     return FileResponse(path, media_type="audio/mpeg", headers={"Accept-Ranges": "bytes"})
 
-
-@router.get("/songs/list")
-def list_songs():
-    files = []
-    for root, _, filenames in os.walk(songs_dir):
-        for f in filenames:
-            if f.endswith(".mp3"):
-                rel_path = os.path.relpath(os.path.join(root, f), songs_dir)
-                files.append(rel_path)
-    return {"songs": files}
-
-
-"""
 @router.get("/songs2")
 def get_song2(file: str = Query(..., description="Nome do arquivo .mp3")):
     path = os.path.join(songs_dir, file)
